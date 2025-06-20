16bf8de90e7baab591526e137fa0e3256afc8005
jaranchai <jaranchai.nt@hotmail.com>
2025-06-20 11:45:35 +0700

    added: initial project

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Sample Web Socket
+
+## Service
+
+install dependencies:
+
+```bash
+pnpm install
+```
+
+create an `.env` file with the following:
+
+```bash
+cd service
+cp .env.sample .env
+```
---
+++ service/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import { createServer } from 'node:http'
+import 'dotenv/config'
+
+const app = express()
+const server = createServer(app)
+
+const PORT = process.env.PORT ?? 3210
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world!!!</h1>')
+})
+
+server.listen(PORT, () => {
+  console.log(`server running at http://localhost:${PORT}`)
+})
