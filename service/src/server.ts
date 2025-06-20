b70c27c4b63e0e4cabea5c3a19ff81cd613b685e
jaranchai <jaranchai.nt@hotmail.com>
2025-06-20 11:54:32 +0700

    added: install cors libs

--- service/src/server.ts
+++ service/src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import { createServer } from 'node:http'
+import cors from 'cors'
+
 import 'dotenv/config'
 
 const app = express()
 const server = createServer(app)
 
 const PORT = process.env.PORT ?? 3210
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world!!!</h1>')
