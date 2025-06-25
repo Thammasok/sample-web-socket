d5e879db25b7a7694132b71940772678702b8dba
jaranchai <jaranchai.nt@hotmail.com>
2025-06-25 16:05:22 +0700

    added app chat page

--- chat-app/src/router.tsx
+++ chat-app/src/router.tsx
@@ -2,11 +2,16 @@ import { createBrowserRouter } from 'react-router-dom'
 import HomePage from '@/app/home/page'
 import LoginPage from '@/app/login/page'
 import RegisterPage from './app/register/page'
+import AppChatPage from './app/app/page'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/app',
+    element: <AppChatPage />,
   },
   {
     path: '/login',
