5352a09e1188a73aa35339948c069bc08e77d295
jaranchai <jaranchai.nt@hotmail.com>
2025-06-25 17:13:22 +0700

    edited button name to create new chat

--- chat-app/src/components/nav-main.tsx
+++ chat-app/src/components/nav-main.tsx
@@ -54,7 +54,7 @@ export function NavMain() {
               className='bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear'
             >
               <IconCirclePlusFilled />
-              <span>Create Chat</span>
+              <span>Create new Chat</span>
             </SidebarMenuButton>
             <Button
               size='icon'
