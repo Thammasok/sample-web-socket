fa5c271bb5d2a2a133f81817ba7bab69be7d5947
jaranchai <jaranchai.nt@hotmail.com>
2025-06-25 15:47:12 +0700

    edited login method to post

--- service/src/rest/api.ts
+++ service/src/rest/api.ts
@@ -10,7 +10,7 @@ export default function api(app: Express, io: SocketIO): void {
 
   // Hello
   router.get('/v1', HelloController.hello)
-  router.get('/v1/login', authLogin)
+  router.post('/v1/login', authLogin)
   router.post('/v1/register', authRegister)
   router.get('/v1/connection-info', (req, res, next) =>
     ChatController.chatConnection(req, res, next, io),
