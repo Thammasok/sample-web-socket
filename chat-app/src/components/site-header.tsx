8e33c3a788b96e4e82d5b50753b0e966015a2e72
jaranchai <jaranchai.nt@hotmail.com>
2025-06-25 21:03:02 +0700

    edited right content component

--- chat-app/src/components/site-header.tsx
+++ chat-app/src/components/site-header.tsx
@@ -4,9 +4,10 @@ import { SidebarTrigger } from '@/components/ui/sidebar'
 
 interface ISiteHeaderProps {
   title?: string
+  rigthContent?: React.ReactNode
 }
 
-export function SiteHeader({ title }: Readonly<ISiteHeaderProps>) {
+export function SiteHeader({ title, rigthContent }: Readonly<ISiteHeaderProps>) {
   return (
     <header className='flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)'>
       <div className='flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6'>
@@ -14,16 +15,18 @@ export function SiteHeader({ title }: Readonly<ISiteHeaderProps>) {
         <Separator orientation='vertical' className='mx-2 data-[orientation=vertical]:h-4' />
         <h1 className='text-base font-medium'>{title}</h1>
         <div className='ml-auto flex items-center gap-2'>
-          <Button variant='ghost' asChild size='sm' className='hidden sm:flex'>
-            <a
-              href='https://github.com/Thammasok/sample-web-socket'
-              rel='noopener noreferrer'
-              target='_blank'
-              className='dark:text-foreground'
-            >
-              GitHub
-            </a>
-          </Button>
+          {rigthContent ?? (
+            <Button variant='ghost' asChild size='sm' className='hidden sm:flex'>
+              <a
+                href='https://github.com/Thammasok/sample-web-socket'
+                rel='noopener noreferrer'
+                target='_blank'
+                className='dark:text-foreground'
+              >
+                GitHub
+              </a>
+            </Button>
+          )}
         </div>
       </div>
     </header>
