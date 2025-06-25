15e6acac62d4b419bfe94bc8f9c4a5cb75748ba9
jaranchai <jaranchai.nt@hotmail.com>
2025-06-25 19:09:09 +0700

    edited change width full screen

--- chat-app/src/app/404/page.tsx
+++ chat-app/src/app/404/page.tsx
@@ -1,6 +1,6 @@
 export default function NotFoundPage() {
   return (
-    <div className='flex flex-col gap-4 justify-center items-center h-screen w-screen'>
+    <div className='flex flex-col gap-4 justify-center items-center h-screen w-full'>
       <img src='/not-found-page.png' alt='ChitChat' className='w-40 h-40' />
       <h1 className='text-6xl font-bold'>Page not found</h1>
       <p className='text-lg text-gray-400'>Look like you're lost. or We not Implement it yet.</p>
