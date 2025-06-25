49a99c58e1138bcda6c439e7055c5dd5efe20bf3
jaranchai <jaranchai.nt@hotmail.com>
2025-06-25 17:27:51 +0700

    added app layout

---
+++ chat-app/src/app/app/layout.tsx
@@ -0,0 +1,25 @@
+import { AppSidebar } from '@/components/app-sidebar'
+import { SidebarProvider } from '@/components/ui/sidebar'
+import useAuthGuard from '@/hooks/use-auth-guard'
+
+interface IAppLayoutProps {
+  children: React.ReactNode
+}
+
+export default function AppLayout({ children }: Readonly<IAppLayoutProps>) {
+  useAuthGuard()
+
+  return (
+    <SidebarProvider
+      style={
+        {
+          '--sidebar-width': 'calc(var(--spacing) * 72)',
+          '--header-height': 'calc(var(--spacing) * 12)',
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant='inset' />
+      {children}
+    </SidebarProvider>
+  )
+}
